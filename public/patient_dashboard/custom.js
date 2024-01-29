85e49be4a52a4fa46cb5bd236b0dbb3c2de8eadf
jay-radadiya-tatva <jay.radadiya@etatvasoft.com>
2024-01-29 18:35:35 +0530

    dashboard update

--- public/patient_dashboard/custom.js
+++ public/patient_dashboard/custom.js
@@ -6,3 +6,43 @@ document.getElementById('dark-mode-button').addEventListener('click',()=>{
         document.documentElement.setAttribute('data-bs-theme','dark')
     }
 })
+
+
+$(function () {
+    var isXS = false,
+        $accordionXSCollapse = $('.accordion-xs-collapse');
+
+    var timer;
+    $(window).resize(function () {
+        if (timer) { clearTimeout(timer); }
+        timer = setTimeout(function () {
+            isXS = window.innerWidth <= 767;
+
+            if (isXS) {
+                $accordionXSCollapse.addClass('collapse');
+            } else {
+                $accordionXSCollapse.removeClass('collapse');
+            }
+        }, 0);
+    }).trigger('resize');
+
+
+    $(document).on('click', '.accordion-xs-toggle', function (e) {
+        e.preventDefault();
+
+        var $thisToggle = $(this),
+            $targetRow = $thisToggle.parent('.tr'),
+            $targetCollapse = $targetRow.find('.accordion-xs-collapse');
+
+        if (isXS && $targetCollapse.length) {
+            var $siblingRow = $targetRow.siblings('.tr'),
+                $siblingToggle = $siblingRow.find('.accordion-xs-toggle'),
+                $siblingCollapse = $siblingRow.find('.accordion-xs-collapse');
+
+            $targetCollapse.collapse('toggle'); // Toggle this collapse
+            $siblingCollapse.collapse('hide'); // Close siblings
+
+            
+        }
+    });
+});
